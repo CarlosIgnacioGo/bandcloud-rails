3db584a8386bd2a16acf96e81496423c410cc9f6
Carlos Gomez <carlos221519@gmail.com>
2016-08-03 00:32:01 -0400

    CRUD para instruments

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'pages/index'
 
+  resources :instruments
+
   devise_for :users, controllers: {
     registrations: 'users/registrations', 
     sessions: 'users/sessions'
